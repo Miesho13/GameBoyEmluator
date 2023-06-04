10306de85c3ae34a9dc3a0169c0c6c7f7f0b32fb
Miesho <marcin.ryzewski.job@gmail.com>
2023-06-04 20:57:21 +0200

    Init base src tree of project

---
+++ src/core/cpu.cc
@@ -0,0 +1,10 @@
+
+/**
+ * This file is part of a the project: "GameBoyEmulator" 
+ * released on the 2.0 version of the Apache License.
+ * 
+ * \author Marcin Ryzewski
+ * \date 06.2023
+*/
+
+#include "../inc/core/cpu.hpp"
---
+++ src/inc/core/cpu.hpp
@@ -0,0 +1,14 @@
+/**
+ * This file is part of a the project: "GameBoyEmulator" 
+ * released on the 2.0 version of the Apache License.
+ * 
+ * \author Marcin Ryzewski
+ * \date 06.2023
+*/
+
+#ifndef __CPU_HPP__
+#define __CPU_HPP__
+
+
+
+#endif
